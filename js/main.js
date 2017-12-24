4e7a98d6796929aec4741aa0918f50731e4fb059
Adi Eyal <adi@openup.org.za>
2017-12-24 08:56:33 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,70 @@
+
+jQuery(document).ready(function($) {
+    var generate_content = function() {
+		var add_entry = function(node, entry) {
+			var tmpl = $.templates("#templ_timeline_block"); // Get compiled template
+			var icon = "img/cd-icon-picture.svg"
+			if (entry.gsx$icon.$t) {
+				icon = entry.gsx$icon.$t;
+			}
+			var data = {
+				title: entry.gsx$title.$t,
+				content: entry.gsx$content.$t,
+				"date" : entry.gsx$date.$t,
+				"icon" : icon
+			};
+			console.log(data)
+			var html = tmpl.render(data);
+			node.append(html);
+		}
+        var spreadsheetID = "11pyYoJQWENhCrVGDMy651ETIXDnW0kdGJnTjhlj1MEg";
+        var url = "https://spreadsheets.google.com/feeds/list/" + spreadsheetID + "/od6/public/values?alt=json";
+        var timeline_block = $("#cd-timeline");
+
+        $.getJSON(url, function(data) {
+            var entry = data.feed.entry;
+
+            $(entry).each(function() {
+                add_entry(timeline_block, this)
+            });
+
+			if (on_blocks_loaded) {
+				on_blocks_loaded();
+			}
+        });
+
+    };
+
+	var on_blocks_loaded = function() { 
+		var timelineBlocks = $('.cd-timeline-block'),
+			offset = 0.8;
+		console.log(timelineBlocks);
+
+		//hide timeline blocks which are outside the viewport
+		hideBlocks(timelineBlocks, offset);
+
+		//on scolling, show/animate timeline blocks when enter the viewport
+		$(window).on('scroll', function(){
+		
+			(!window.requestAnimationFrame) 
+				? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+				: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+		});
+
+		function hideBlocks(blocks, offset) {
+			blocks.each(function(){
+				( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+			});
+		}
+
+		function showBlocks(blocks, offset) {
+			blocks.each(function(){
+			console.log("yo1");
+				( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+			});
+		}
+	}
+
+    generate_content();
+
+});
